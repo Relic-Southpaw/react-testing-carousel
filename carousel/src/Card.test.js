3909eabc28bc4921c939e28ff21987743b86f47e
Relic <RobertSmithTheFourth@Outlook.com>
2023-08-17 17:45:30 -0400

    I think I'm finished but it's not running the script, I think there's compatibility issues

--- carousel/src/Card.test.js
+++ carousel/src/Card.test.js
@@ -5,3 +5,10 @@ import Card from "./Card";
 test(" it renders without crashing", function () {
     render(<Card />);
 });
+
+
+test("function matches the snapshot", function () {
+    const { asFragment } = render(<Card />);
+    expect(asFragment()).toMatchSnapshot();
+
+})
--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -2,9 +2,14 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
-it("renders without crashing", function () {
+test("Carousel renders without crashing", function () {
   render(<Carousel />);
 });
+
+test("the fragment matches snapshot", function () {
+  const { asFragment } = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+})
 
 
 it("works when you click on the right arrow", function () {
